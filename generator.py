386f9ebfb374c91ddcf3fd4f249b79bed6d6e0ac
Leo346346 <64582399+Leo346346@users.noreply.github.com>
2023-08-10 08:46:07 +0200

    Create generator.py

---
+++ generator.py
@@ -0,0 +1,64 @@
+# #Random Password generator
+import os
+import string
+import random
+
+def main():
+    while(True):
+        #clears screen
+        os.system('cls')
+        #gives us the length of the password
+        length = int(input('How long do you want the password: '))
+        print('''Choose which character sets you want from this list, input their number to choose them
+              1. Digits
+              2. Letters
+              3. Special characters
+              4. Get Passsword with my choices
+                ''')
+
+        characters = ''
+        #choices in what your password should contain
+        while(True):
+            choice = int(input('Write Your number: '))
+            if(choice == 1):
+                #gives numbers
+                characters += string.digits
+            elif(choice == 2):
+                #gives normal letters
+                characters += string.ascii_letters
+            elif(choice == 3):
+                #gives special characters
+                characters += string.punctuation
+            elif(choice == 4):
+                break
+            else:
+                print('Something went wrong, try again')
+                
+            password = []
+
+        for i in range(length):
+            randomchar = random.choice(characters)
+
+            password.append(randomchar)
+
+        print(f'Your password is: ' + ''.join(password))
+        
+        #just there to see if you input either an y or n
+        while(True):
+            again = str.lower(input('Want to try again= [y/n]'))
+            if(again == 'y'):
+                break
+            elif(again == 'n'):
+                break
+            else:
+                print('try inputting a "y" or a "n"')
+
+        #says if the outer loop should continue
+        if(again == 'y'):
+            continue
+        elif(again == 'n'):
+            break
+        
+
+if __name__ == "__main__":
+    main()
